3c050b0bb22e69976d408b8217c473affaac78f1
Oleksandr Solokha <oleksandr.solokha@gmail.com>
2020-07-15 16:56:38 +0300

    Initial Commit

---
+++ TestUISwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestUISwift
+//
+//  Created by Oleksandr Solokha on 15.07.2020.
+//  Copyright © 2020 Oleksandr Solokha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
